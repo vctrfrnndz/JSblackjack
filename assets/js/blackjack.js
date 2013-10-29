3233d42a7d5cc5d952d4c3799d79d26a72bc8c9b
Victor Fernandez <victorhf@gmail.com>
2013-10-28 22:02:31 -0300

    Updated GH pages

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -1,379 +1,351 @@
-$(function(){
+/**
+ * JSblackjack 0.2.1 (https://github.com/vctrfrnndz/JSblackjack)
+ * (c) 2012-2013 Victor Fernandez <victor@vctrfrnndz.com>
+ * MIT Licensed.
+ */
 
-// DOM REFERENCES & GLOBALS
-// ========================
-var suits = ["club", "diamond", "heart", "spade"],
-    cardValue = ["a",2,3,4,5,6,7,8,9,10,"j","q","k"],
-    loader = document.getElementById('loading'),
-    cover = document.getElementById('cover'),
-    game = document.getElementById('game'),
-    fe_player = document.getElementById('player'),
-    fe_dealer = document.getElementById('dealer'),
-    fe_msg = document.getElementById('msg'),
-    controls = document.getElementById('controls'),
-    msg,
-    userHand,
-    dealerHand,
-    hiddenCard,
-    i = 0;
+// TODO
+// - Move away from jquery/transit dependency
+// - Replace global variables for options object
+// - Make score status updates only after the cards are visually dealed
+// - Implement betting
 
-// DEAL
-// ====
+var Card, Game, Hand, UserException, barContainer, cardValue, controlsContainer, dealerContainer, dealerHand, hiddenCard, playerContainer, playerHand, suits;
 
-var deal = function(){
-    var cardSuit = Math.floor(Math.random()*4);
-    var cardNumber = Math.floor(Math.random()*13);
+suits = ["club", "diamond", "heart", "spade"];
 
-    return new Card(cardSuit, cardNumber);
+cardValue = ["a", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "q", "k"];
+
+playerContainer = "#player";
+
+dealerContainer = "#dealer";
+
+controlsContainer = "#controls";
+
+barContainer = "#msg";
+
+Game = {
+    deal: function () {
+        var cardNumber, cardSuit;
+
+        cardSuit = Math.floor(Math.random() * suits.length);
+        cardNumber = Math.floor(Math.random() * cardValue.length);
+
+        return new Card(cardSuit, cardNumber);
+    },
+
+    hitUser: function (role) {
+        var cardObject, newCard, userContainer;
+
+        if (role === 'player') {
+            cardObject = playerHand.hitMe();
+            userContainer = playerContainer;
+        }
+        else if (role === 'dealer') {
+            cardObject = dealerHand.hitMe();
+            userContainer = dealerContainer;
+        }
+        else {
+            throw new UserException('hitUser function needs a role');
+        }
+
+        newCard = cardObject.getCard();
+
+        $(newCard).appendTo(userContainer).addClass('placed');
+    },
+
+    createHand: function (role) {
+        var card, newHand, userCard, userCards, userContainer, i, len;
+
+        newHand = new Hand();
+        userCards = newHand.getHand();
+        userContainer = null;
+
+        if (role === 'player') {
+            userContainer = playerContainer;
+        }
+        else if (role === 'dealer') {
+            userContainer = dealerContainer;
+        }
+        else {
+            throw new UserException('createHand function needs a role');
+        }
+        for (i = 0, len = userCards.length; i < len; i++) {
+            card = userCards[i];
+            userCard = userCards[i].getCard();
+            if (role === 'dealer' && i === 0) {
+                userCard = userCards[i].getCard('hidden');
+                hiddenCard = userCards[i].getCard();
+            }
+            else {
+                userCard = userCards[i].getCard();
+            }
+            $(userContainer).append(userCard);
+        }
+        if (role === 'player') {
+            Game.helpers.updateBar('Your hand is '+ newHand.score() + ', press Hit or Stand');
+        }
+
+        return newHand;
+    },
+
+    declareWinner: function (playerHand, delaerHand) {
+        var dealerScored, gameScore, playerScored;
+
+        playerScored = 'You got ' + playerHand.score();
+        dealerScored = 'dealer got ' + dealerHand.score();
+        gameScore = playerScored + ', ' + dealerScored;
+
+        if (playerHand.score() === 21) {
+            return playerScored + '! Blackjack!';
+        }
+        else if (playerHand.score() > 21) {
+            if (dealerHand.score() > 21) {
+                return gameScore + '. You tied!';
+            }
+            else {
+                return gameScore + '. You lose!';
+            }
+        }
+        else if (dealerHand.score() > 21) {
+            return gameScore + '. You win!';
+        }
+        else if (playerHand.score() > dealerHand.score()) {
+            return gameScore + '. You win!';
+        }
+        else if (playerHand.score() === dealerHand.score()) {
+            return gameScore + '. You tied!';
+        }
+        else {
+            return gameScore + '! You lose!';
+        }
+    },
+
+    helpers: {
+        dealAnimate: function () {
+            $('.hand').each(function () {
+                var counter, currentCard, currentHand, animation;
+                counter = 0;
+                currentHand = $(this);
+                currentCard = currentHand.find(".card");
+                animation = setInterval(function () {
+                    $(currentCard[counter]).addClass('placed');
+                    if (counter === currentCard.length) {
+                        clearInterval(this);
+                    }
+                    counter++;
+                }, 200);
+            });
+        },
+
+        updateBar: function (message) {
+            $(barContainer).text(message);
+        },
+
+        stopControls: function () {
+            $(controlsContainer).find("#hit, #stand").addClass('dis');
+        }
+    },
+
+    play: {
+        finish: function () {
+            Game.helpers.stopControls();
+            $(dealerContainer).find(".card:first-child").remove();
+            $(hiddenCard).prependTo(dealerContainer).addClass('rolled');
+            Game.helpers.updateBar(Game.declareWinner(playerHand, dealerHand));
+            $(controlsContainer).find("#restart").show();
+        },
+
+        evaluate: function () {
+            if (playerHand.score() >= 21) {
+                Game.play.finish();
+            }
+            else if (playerHand.score() < 21) {
+                Game.hitUser('player');
+
+                if (dealerHand.score() < 17) {
+                    Game.hitUser('dealer');
+
+                    if (dealerHand.score() >= 21) {
+                        Game.play.finish();
+                    }
+                }
+                if (playerHand.score() >= 21 || dealerHand.score() >= 21) {
+                    Game.play.finish();
+                }
+                else {
+                    Game.helpers.updateBar('Your hand is ' + playerHand.score() + ', press Hit or Stand');
+                }
+            }
+            else {
+                Game.play.finish();
+            }
+        }
+    },
+
+    startScreen: function() {
+        $('#cover').css('visibility', 'visible');
+
+        $('#logo')
+            .transition({ x: '-50em' }, 0)
+            .transition({ x: '0' }, 600, 'in-out' );
+        $('#new-game')
+            .transition({ x: '-50em' }, 0);
+
+        $('#new-game')
+            .transition({ x: '0' }, 600, 'in-out');
+        $('#logo')
+            .transition({ perspective: '439px', rotateY: '0deg' })
+            .transition({ perspective: '439px', rotateY: '360deg' }, 600);
+    },
+
+    startNew: function () {
+        playerHand = Game.createHand('player');
+        dealerHand = Game.createHand('dealer');
+
+        Game.helpers.dealAnimate();
+
+        if (playerHand.score() >= 21) {
+            Game.play.finish();
+        }
+    },
+
+    init: function () {
+        Game.startScreen();
+
+        $("#new-game").on('click', function () {
+            Game.startNew();
+            $("#cover").hide();
+            $('#game').css('visibility', 'visible');
+            return false;
+        });
+
+        $(controlsContainer).on('click', '#hit:not(.dis)', function () {
+            Game.play.evaluate();
+            return false;
+        });
+
+        $(controlsContainer).on('click', '#stand:not(.dis)', function () {
+            Game.play.finish();
+            return false;
+        });
+
+        $(controlsContainer).on('click', '#restart', function () {
+            $(".card").remove();
+            $(controlsContainer).find('#hit, #stand').removeClass('dis');
+            $(controlsContainer).find('#restart').hide();
+            Game.startNew();
+            return false;
+        });
+    }
 };
- 
-// CARD
-// ====
 
-var Card = function(cardSuit, cardNumber){
-    var suit = cardSuit,
-        number = cardNumber,
-        color = (suit === 1 || suit === 2) ? 'red' : 'black';
+Card = function (cardSuit, cardNumber) {
+    var color, number, suit;
 
-    this.getNumber = function(){
+    suit = cardSuit;
+    number = cardNumber;
+    color = suit === 1 || suit === 2 ? 'red' : 'black';
+
+    this.getNumber = function () {
         return number;
     };
 
-    this.getLetter = function(){
-        return cardValue[number];
-    };
-
-    this.getSuit = function(){ 
+    this.getSuit = function () {
         return suit;
     };
 
-    this.getValue = function(){ 
-        if (number === 1) { 
-            return 11; 
+    this.getLetter = function () {
+        return cardValue[number];
+    };
+
+    this.getValue = function () {
+        if (number === 1) {
+            return 11;
         }
-        else if (number > 1 && number < 10) { 
+        else if (number > 1 && number < 10) {
             return number;
         }
-        else { 
+        else {
             return 10;
         }
     };
 
-    this.getCard = function(){
-        var card = [];
+    this.getCard = function (type) {
+        var card;
 
-        card = '<div class="' + "card ";
-        card += suits[suit] + " " + color + " card-" + cardValue[number];
-        card += '"'+ ' data-number="' + cardValue[number] + '">';
-        card += '<div class="cover"></div><div class="deck"></div>';
-        card += '</div>';
+        if (type === 'hidden') {
+            card = '<div class="card unknown"></div>';
+        }
+        else {
+            card = '<div class="' + "card ";
+            card += suits[suit] + " " + color + " card-" + cardValue[number];
+            card += '"' + ' data-number="' + cardValue[number] + '">';
+            card += '<div class="cover"></div><div class="deck"></div>';
+        }
 
         return card;
     };
 };
 
-// HAND
-// ====
+Hand = function () {
+    var card1, card2, handCards;
 
-var Hand = function(){
-    var card1 = deal(), 
-        card2 = deal(),
-        cards = [card1, card2];
+    card1 = Game.deal();
+    card2 = Game.deal();
+    handCards = [card1, card2];
 
-    this.getCard1 = function(){
+    this.getFirstCard = function () {
         return card1;
     };
 
-    this.getCard2 = function(){
+    this.getSecondCard = function () {
         return card2;
     };
 
-    this.getHand = function(){
-        return cards;
+    this.getHand = function () {
+        return handCards;
     };
 
-    this.score = function(){
-        var sum = 0,
-            aces = 0;
+    this.score = function () {
+        var aces, card, sum, i, len;
 
-        for (i=0; i<cards.length; i++){
-            sum += cards[i].getValue();
+        sum = 0;
+        aces = 0;
+
+        for (i = 0, len = handCards.length; i < len; i++) {
+            card = handCards[i];
+            sum += handCards[i].getValue();
             sum += 1;
 
-            if (cards[i].getValue()===11){
-                aces+=1;
+            if (handCards[i].getValue() === 11) {
+                aces += 1;
             }
         }
 
-        while(aces > 0 && sum > 21){
-            sum-=10;
+        while (aces > 0 && sum > 21) {
+            sum -= 10;
             aces--;
         }
 
         return sum;
     };
 
-    this.hitMe = function(){
-        var hitCard = deal();
+    this.hitMe = function () {
+        var randomCard;
 
-        cards.push(hitCard);
+        randomCard = Game.deal();
+        handCards.push(randomCard);
 
-        return hitCard;
+        return randomCard;
     };
 };
- 
-function dealer(){
-    var dealer = new Hand(),
-        dealerCards = dealer.getHand(),
-        dealerCard = [];
 
-        for(i = 0; i < dealerCards.length; i++){
-            if (i === 0) {
-                dealerCard = '<div class="card unknown"></div>';
-
-                hiddenCard = dealerCards[i].getCard();
-            }
-            else {
-                dealerCard = dealerCards[i].getCard();
-            }
-
-            $(fe_dealer).append(dealerCard);
-        }
-
-    return dealer;
-}
- 
-function user(){
-    var user = new Hand(),
-        userCards = user.getHand();
-
-        for(i = 0; i < userCards.length; i++) {
-            $(fe_player).append(userCards[i].getCard());
-        }
-
-    updateBar("Your hand is "+ user.score() + ", press Hit or Stand");
-
-    return user;
-}
-
-function winner(userHand, dealerHand){
-    var yourScore = "You got " + userHand.score(),
-        dealerScore = "dealer got " + dealerHand.score(); 
-
-    if (userHand.score() > 21){
-        if(dealerHand.score() > 21){
-            return yourScore + ", " + dealerScore + ". You tied!";
-        }
-        else{
-            return yourScore + ", " + dealerScore + ". You lose!";
-        }
-    }
-
-    else if(dealerHand.score() > 21){
-        return yourScore + ", " + dealerScore + ". You win!";
-    }
-
-    else if(userHand.score() > dealerHand.score()){
-        return yourScore + ", " + dealerScore + ". You win!";
-    }
-
-    else if(userHand.score() === 21){
-        return yourScore + "! Blackjack!";
-    }
-
-    else if(userHand.score() === dealerHand.score()){
-        return yourScore + ", " + dealerScore + ". You tied!";
-    }
-
-    else{
-        return yourScore + ", " + dealerScore + ". You lose!";
-    }
-}
-
-function stopGame() {
-    $(controls).find('#hit').addClass('dis').off();
-    $(controls).find('#stand').addClass('dis').off();
-}
-
-function updateBar(msg) {
-    $(fe_msg).text(msg);
-}
-
-function scoreGame() {
-    stopGame();
-
-    $('#dealer .card:first-child').remove();
-    $(hiddenCard).prependTo('#dealer').addClass('rolled');
-
-    updateBar(winner(userHand,dealerHand));
-    $(controls).find('#restart').show();
-}
-
-function playGame(){
-    userHand = user();
-    dealerHand = dealer();
-}
-
-function startOver(){
-    $(controls).find('#hit').removeClass('dis').on();
-    $(controls).find('#stand').removeClass('dis').on();
-
-    playGame();
-
-    $(controls).find('#restart').hide();
-}
-
-function hitPlayer(){
-    var hitPlayer = userHand.hitMe(),
-        newCard = hitPlayer.getCard();
-
-    $(newCard).appendTo(fe_player).addClass('placed');
-    
-}
-
-
-function hitDealer(){
-    var hitDealer = dealerHand.hitMe(),
-        newCard = hitDealer.getCard();
-
-    $(newCard).appendTo(fe_dealer).addClass('placed');
-}
-
-// ANIMATIONS
-// ==========
-
-var coverAnimate = function () {
-    cover.style.visibility = 'visible';
-
-    // ELEMENTS IN
-    $('#logo')
-        .transition({ x: '-50em' }, 0)
-        .transition({ x: '0' }, 600, 'in-out' );
-    $('#new-game')
-        .transition({ x: '-50em' }, 0);
-
-    // ANIMATE ELEMENTS
-
-    $('#new-game')
-        .transition({ x: '0' }, 600, 'in-out');
-    $('#logo')
-        .transition({ perspective: '439px', rotateY: '0deg' })
-        .transition({ perspective: '439px', rotateY: '360deg' }, 600);
-        
+UserException = function (message) {
+    this.message = message;
+    this.name = 'UserException';
 };
 
-var dealAnimate = function() {
-    
-    $('.hand').each(function() {
-        var i = 0,
-            currentHand = $(this),
-            currentCard = currentHand.find('.card'),
-            addCard;
-
-        addCard = setInterval(function() {
-            $(currentCard[i]).addClass('placed');
-
-            if(i === currentCard.length) {
-                clearInterval(this);
-            }
-
-            i++;
-        }, 200);
-    });
-};
-
-
-// LOADER 
-// ======
-
-var loadingPoints = 0;
-
-var loading = setInterval(function() {
-
-    $('#loading .text .dots').append('.');
-
-    if (loadingPoints > 2) {
-        $('#loading .text .dots').html('');
-
-        loadingPoints = 0;
-    }
-    else {
-        loadingPoints++;
-    }
-
-}, 180);
-
-setTimeout(function() {
-    clearInterval(loading);
-    $(loader).fadeOut();
-    coverAnimate();
-},1200);
-
-// START NEW GAME 
-// ==============
-
-playGame();
-
-$('#new-game').on('click', function(e) {
-    e.preventDefault();
-
-    $(cover).hide();
-
-    game.style.visibility = 'visible';
-
-    dealAnimate();
-
-    var playerCards = $(fe_player).children(),
-        dealerCards = $(fe_dealer).children();
-
-    if (userHand.score() >= 21) {
-        scoreGame();
-    }
-
-    $(controls).on('click', '#hit', function(e) {
-        e.preventDefault();
-
-        if (userHand.score() < 21) {
-            hitPlayer();
-
-            if(dealerHand.score() < 17) {
-                hitDealer();
-
-                if (dealerHand.score() >= 21) {
-                    scoreGame();
-                }
-            }
-
-            if(userHand.score() >= 21) {
-                scoreGame();
-            } 
-
-            else {
-                updateBar("Your hand is " + userHand.score() + ", press Hit or Stand");
-            }
-        }
-
-        else {
-            scoreGame();
-        }
-    });
-
-    $(controls).on('click', '#stand', function(e) {
-        e.preventDefault();
-
-        scoreGame();
-    });
-
-    $(controls).on('click', '#restart', function(e) {
-        e.preventDefault();
-
-        $('.hand .card').remove();
-
-        startOver();
-        dealAnimate();
-    });
-
-
-});
-
+$(document).ready(function () {
+    Game.init();
 });
